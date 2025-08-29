1065eeacbd6cac9521268ed3babc0f702777e338
jatisusan <susurrus112@gmail.com>
2025-08-29 12:22:34 +0545

    Setup upstash redis and implement ratelimit

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,13 @@
+
+const page = () => {
+  return (
+    <main className="root-container flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-light-100 font-bebas-neue text-5xl font-bold">
+        Whoa, slow down!
+      </h1>
+      <p className="text-light-400 max-w-xl text-center mt-3">You've exceeded the allowed request limit. Please wait a moment and try again later.</p>
+    </main>
+  )
+}
+
+export default page
